88fc58ac3afff586f204a537fdd5cf42285e44ff
yadu <yaduhat@gmail.com>
2020-07-04 12:16:27 -0500

    data model of BeerDto and BeerController with all Spring Pagable List

---
+++ src/main/java/com/sbmssc/brawerry/app/sbmsscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,36 @@
+package com.sbmssc.brawerry.app.sbmsscbeerservice.web.controller;
+
+import com.sbmssc.brawerry.app.sbmsscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        //do impl
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        //to impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+    @PutMapping("/{beerId}")
+
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+        //do impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+    @DeleteMapping("/{beerId}")
+    public ResponseEntity deleteBeerById(@PathVariable("beerId") UUID beerId){
+        //do impl
+        return  new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/sbmssc/brawerry/app/sbmsscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.sbmssc.brawerry.app.sbmsscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, WHEAT,PALE_ALE
+}
