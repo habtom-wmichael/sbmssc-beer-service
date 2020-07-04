b80ada895ab62becb621ad34811494b95a9b6c4d
yadu <yaduhat@gmail.com>
2020-07-04 11:12:13 -0500

    intail project structure commit

---
+++ src/main/java/com/sbmssc/brawerry/app/sbmsscbeerservice/SbmsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sbmssc.brawerry.app.sbmsscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsscBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbmsscBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sbmssc/brawerry/app/sbmsscbeerservice/SbmsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbmssc.brawerry.app.sbmsscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
