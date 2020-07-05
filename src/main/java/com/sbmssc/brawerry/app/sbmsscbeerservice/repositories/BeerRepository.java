a9978bf24804c8ba3d1c69cfcf85cc6afc2f73f8
yadu <yaduhat@gmail.com>
2020-07-04 23:15:13 -0500

    Beer Repository has added and configured

---
+++ src/main/java/com/sbmssc/brawerry/app/sbmsscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.sbmssc.brawerry.app.sbmsscbeerservice.repositories;
+
+import com.sbmssc.brawerry.app.sbmsscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+//you don't need to add @Repository annotation
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
