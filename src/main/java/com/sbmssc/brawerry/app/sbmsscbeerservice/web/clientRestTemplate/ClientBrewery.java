23d6450f8c1d05321522cfe9e5d76f8a07d3d599
yadu <yaduhat@gmail.com>
2020-07-04 23:05:04 -0500

    Beer Entiry is added and configured

--- src/main/java/com/sbmssc/brawerry/app/sbmsscbeerservice/web/clientRestTemplate/ClientBrewery.java
+++ src/main/java/com/sbmssc/brawerry/app/sbmsscbeerservice/web/clientRestTemplate/ClientBrewery.java
@@ -1,4 +1,33 @@
 package com.sbmssc.brawerry.app.sbmsscbeerservice.web.clientRestTemplate;
 
+import com.sbmssc.brawerry.app.sbmsscbeerservice.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+@ConfigurationProperties(value = "mssc.brewery", ignoreInvalidFields = false)
 public class ClientBrewery {
+    public final String BEER_PATH_V1="/app/v1/beer";
+
+   private RestTemplate restTemplate;
+    private  String apihost;
+
+    public ClientBrewery() {
+    }
+
+    public ClientBrewery(String apihost) {
+        this.apihost = apihost;
+    }
+
+    public ClientBrewery(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+    public BeerDto getBeerById(UUID uuid){
+        return restTemplate.getForObject(apihost+BEER_PATH_V1+uuid,BeerDto.class);
+    }
+
 }
--- src/test/java/com/sbmssc/brawerry/app/sbmsscbeerservice/web/clientRestTemplate/ClientBreweryTest.java
+++ src/test/java/com/sbmssc/brawerry/app/sbmsscbeerservice/web/clientRestTemplate/ClientBreweryTest.java
@@ -1,5 +1,21 @@
+package com.sbmssc.brawerry.app.sbmsscbeerservice.web.clientRestTemplate;
+
+import com.sbmssc.brawerry.app.sbmsscbeerservice.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class ClientBreweryTest {
-
+@Autowired
+    ClientBrewery clientBrewery;
+    @Test
+    void getBeerById() {
+        BeerDto beerDto= clientBrewery.getBeerById(UUID.randomUUID());
+        assertNotNull(beerDto);
+    }
 }
